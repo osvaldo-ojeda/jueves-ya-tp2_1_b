9fcfce6077910a28202fc7e98260a6c5647213f8
Osvaldin Ojeda <osvaldo.devmate@gmail.com>
2024-09-05 19:32:05 -0300

    clase3 ejercicios

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const decirHola = require("./funciones/decirHola");
 const numeroMayor=require("./funciones/numeroMayor")
 const sumaElementos=require("./funciones/sumaElementos")
-const abbrevName= require("./funciones/abbrevName")
+const abbrevName= require("./funciones/abbrevName");
+const filtradaDeArray = require("./funciones/filtradaDeArray");
+
 
 //  console.log(module)
 // console.log(require)
 
 // decirHola();
 // numeroMayor([3,6,2])
-sumaElementos([20,3,6,2])
-abbrevName("Ricky Martin")
+// sumaElementos([20,3,6,2])
+// abbrevName("Ricky Martin")
+const estudiantes = [
+     { nombre: "Juan", edad: 17, curso: "Primero" },
+     { nombre: "Ana", edad: 18, curso: "Segundo" },
+     { nombre: "Carlos", edad: 16, curso: "Primero" },
+     { nombre: "María", edad: 19, curso: "Tercero" },
+   ];
+filtradaDeArray(estudiantes)
